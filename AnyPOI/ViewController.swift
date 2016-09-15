6879f0472ae487b8caee420d7138b6dd91d4fa61
Sebastien Brugalieres <sebwebnet@free.fr>
2016-09-15 18:20:55 +0200

    Initial Commit

---
+++ AnyPOI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnyPOI
+//
+//  Created by Sébastien Brugalières on 15/09/2016.
+//  Copyright © 2016 Sébastien Brugalières. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
