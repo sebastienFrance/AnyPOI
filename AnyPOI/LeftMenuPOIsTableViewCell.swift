e00f1b426f891a15a3a55ce9ad3d02be65a7803e
Sébastien Brugalières <sebastien@macbook-pro-de-sebastien.local>
2016-10-21 21:37:19 +0200

    Add icons for left menu

--- AnyPOI/LeftMenuPOIsTableViewCell.swift
+++ AnyPOI/LeftMenuPOIsTableViewCell.swift
@@ -1,29 +1,30 @@
 //
-//  LeftMenuTableViewCell.swift
-//  SimplePOI
+//  LeftMenuPOIsTableViewCell.swift
+//  AnyPOI
 //
-//  Created by Sébastien Brugalières on 04/08/2016.
+//  Created by Sébastien Brugalières on 21/10/2016.
 //  Copyright © 2016 Sébastien Brugalières. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class LeftMenuTableViewCell: UITableViewCell {
-
+class LeftMenuPOIsTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var menuTitle: UILabel!
-    @IBOutlet weak var imageMenu: UIImageView!
+    @IBOutlet weak var pinView: MKPinAnnotationView!
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         
         // Configure the view for the selected state
     }
-
+    
 }
--- AnyPOI/LeftMenuViewController.swift
+++ AnyPOI/LeftMenuViewController.swift
@@ -36,19 +36,54 @@ class LeftMenuViewController: UIViewController {
 
 extension LeftMenuViewController : UITableViewDataSource, UITableViewDelegate {
     //MARK: UITableViewDataSource
+    struct Row {
+        static let Map = 0
+        static let POIs = 1
+        static let Route = 2
+        static let Options = 3
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuTitles.count + 1
     }
     
+    struct storyboard {
+        static let LeftMenuPOIsTableViewCellId = "LeftMenuPOIsTableViewCellId"
+        static let LeftMenuCellId = "LeftMenuCellId"
+        static let MenuAboutCellId = "MenuAboutCellId"
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if (indexPath as NSIndexPath).row < menuTitles.count {
-            let theCell = theTableView.dequeueReusableCell(withIdentifier: "LeftMenuCellId", for: indexPath) as! LeftMenuTableViewCell
-            theCell.menuTitle.text = menuTitles[(indexPath as NSIndexPath).row]
-             return theCell
+        if indexPath.row < menuTitles.count {
+            if indexPath.row == Row.POIs {
+                let theCell = theTableView.dequeueReusableCell(withIdentifier: storyboard.LeftMenuPOIsTableViewCellId, for: indexPath) as! LeftMenuPOIsTableViewCell
+                theCell.pinView.animatesDrop = false
+                theCell.pinView.canShowCallout = false
+                theCell.pinView.pinTintColor = UIColor.blue
+                theCell.menuTitle.text = menuTitles[(indexPath as NSIndexPath).row]
+                return theCell
+            } else {
+                
+                let theCell = theTableView.dequeueReusableCell(withIdentifier: storyboard.LeftMenuCellId, for: indexPath) as! LeftMenuTableViewCell
+                theCell.menuTitle.text = menuTitles[(indexPath as NSIndexPath).row]
+                
+                switch indexPath.row {
+                case Row.Map:
+                    theCell.imageMenu.image = UIImage(named: "Geography-40")
+                case Row.Route:
+                    theCell.imageMenu.image = UIImage(named: "Waypoint Map-40")
+                case Row.Options:
+                    theCell.imageMenu.image = UIImage(named: "Settings-40")
+                default:
+                    break
+                }
+                
+                return theCell
+            }
         } else {
-            let theCell =  theTableView.dequeueReusableCell(withIdentifier: "MenuAboutCellId", for: indexPath) as! LeftMenuAboutTableViewCell
+            let theCell =  theTableView.dequeueReusableCell(withIdentifier: storyboard.MenuAboutCellId, for: indexPath) as! LeftMenuAboutTableViewCell
             let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
             let build = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as! String
             
